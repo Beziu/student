e9e0b27883f04357c838bd1bc31586af31b62358
Grzegorz Dziubak <grzegorzdziubak@gmail.com>
2021-09-24 10:31:21 +0200

    Initial commit

---
+++ src/main/java/com/myproject/student/StudentSpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentSpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentSpringbootDemoApplication.class, args);
+	}
+
+}
